f4958d34d5df2507df03548cc34af4a6d770bbed
umarijaz-Dtech <umar.ijaz@dtechsystems.co>
2024-09-19 18:17:34 +0500

    make report correct

--- resources/views/pdf/user-report.blade.php
+++ resources/views/pdf/user-report.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         /* body {
-            font-size: 10px; 
+            font-size: 10px;
         } */
         .table thead th {
             font-size: 10px;
@@ -20,13 +20,13 @@
             width: 100%;
         }
         .table td, .table th {
-            padding: 5px; 
+            padding: 5px;
             vertical-align: middle;
             border-top: 1px solid #dee2e6;
         }
     </style>
 </head>
-<body> 
+<body>
     <div class="">
         <div class="card">
             <div class="card-header">
@@ -55,33 +55,16 @@
                             </thead>
                             <tbody>
                                 @foreach ($company->jobs as $job)
-                                @php
-                                    $createdAt = \Carbon\Carbon::parse($job->created_at);
-                                    $today = \Carbon\Carbon::now();
-                                    $daysBetween = $createdAt->diffInDays($today);
-
-                                    $SoicalReads = 0;
-                                    $AggregatesReads = 0;
-                                    $websiteReads = 0;
-                                    $websiteClicksThrough = 0;
-
-                                    for ($i = 0; $i <= $daysBetween; $i++) {
-                                        $SoicalReads += rand(30, 150);
-                                        $AggregatesReads += rand(45, 285);
-                                        $websiteReads += rand(50, 120);
-                                        $websiteClicksThrough += rand(22, 29);
-                                    }
-                                @endphp
                                 <tr>
                                     <td style="width:20%">
                                         <a href="{{ route('job.show', $job->id) }}" class="company">
                                             <b>{{ $job->title }}</b>
                                         </a>
                                     </td>
-                                    <td class="text-center">{{ $SoicalReads }}</td>
-                                    <td class="text-center">{{ $AggregatesReads }}</td>
-                                    <td class="text-center">{{ $websiteReads }}</td>
-                                    <td class="text-center">{{  $websiteClicksThrough }}</td>
+                                    <td class="text-center">{{ $job->social_media_clicks_through }}</td>
+                                    <td class="text-center">{{ $job->aggregates_clicks_through }}</td>
+                                    <td class="text-center">{{ $job->website_reads }}</td>
+                                    <td class="text-center">{{  $job->website_clicks_through }}</td>
                                     <td>{{ ucfirst($job->status) }}</td>
                                     {{-- <td>{{ date('j F, Y', strtotime($job->created_at)) }}</td> --}}
                                     <td>
